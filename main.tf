1436aa9a5ea9d7955407ee9ff39d33862403e0d5
bassam <bassamkhamis7@gmail.com>
2023-01-11 14:32:25 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+/*
+1. Create VPC
+2. Create internet Gateway
+3. Create Custom Route table
+4. Create a Subnet
+5. Associate the subnet with Route Table
+6. Create security Group to allow port 22,80,443
+7. Create a network interface with an ip in the subnet that was created
+8. Assign an elastic IP to the network interface created in step7
+9. Create Ubuntu server and install/enable apache2
+*/
